7ef0f55fee426b3b9354e1eb2d889da5ef67bc69
theomper <theo.mper@gmail.com>
2022-03-12 18:55:46 +0200

    [major] - completed q3_rdd

--- code/q3_rdd.py
+++ code/q3_rdd.py
@@ -1,6 +1,7 @@
 # code for capturing execution time copied from
 # https://stackoverflow.com/questions/1557571/how-do-i-get-time-of-a-python-programs-execution
 
+from re import L
 from pyspark.sql import SparkSession
 from io import StringIO
 import csv
@@ -17,35 +18,36 @@ spark = SparkSession.builder.appName("q3_rdd").getOrCreate()
 
 sc = spark.sparkContext
 
-# convert ratings => (movieId, rating)
+# map => (user_id, movie_id, rating, timestamp)
+# map => (movieId, (rating, 1))
+# reduce => (movieId, (sum_of_ratings, cnt_of_ratings))
+# map => (movieId, (avg_rating, cnt_of_ratings))
 ratings = \
     sc.textFile("hdfs://master:9000/files/ratings.csv"). \
-    map(lambda x: split_complex(x)).\
-    map(lambda x: (int(x[1]), float(x[2])))
+    map(lambda x: split_complex(x)). \
+    map(lambda x: (int(x[1]), (float(x[2]), 1))). \
+    reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1])). \
+    map(lambda x: (x[0], (x[1][0] / x[1][1], x[1][1])))
 
-# (movieId, genre)
+# map => (movieId, genre)
 movie_genres = \
     sc.textFile("hdfs://master:9000/files/movie_genres.csv"). \
     map(lambda x: split_complex(x)).\
     map(lambda x: (int(x[0]), x[1]))
 
-# results [movieId, (rating, genre)]
-results = ratings.join(movie_genres)
+# join => (movieId, (genre, (avg_rating, cnt_of_ratings)))
+joined = movie_genres.join(ratings)
 
-# possible bug in the reduceByKey -> drops useful data
-avg_rtg_movie = results. \
-    map(lambda x: (x[0], (x[1][0], x[1][1], 1))). \
-    reduceByKey(lambda x, y: (x[0] + y[0], x[1], x[2] + y[2])). \
-    map(lambda x: (x[0], (x[1][1], x[1][0]/x[1][2])))
-
-#  or possible bug in the reduceByKey -> drops useful data
-avg_rtg_genre = avg_rtg_movie. \
-    map(lambda x: (x[1][0], (x[1][1], 1))). \
+# map => (genre, (avg_rating, 1))
+# reduce => (genre, (sum_of_avg_ratings, cnt_of_avg_ratings))
+# map => (genre, (avg_rating_per_genre, cnt_of_avg_rating_per_genre))
+results = joined. \
+    map(lambda x: (x[1][0], (x[1][1][0], 1))). \
     reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1])). \
-    map(lambda x: (x[0], x[1][0]/x[1][1], x[1][1]))
+    map(lambda x: (x[0], (x[1][0]/x[1][1], x[1][1])))
 
 # Print results
-for result in avg_rtg_genre.collect():
+for result in results.collect():
     print(result)
 
 # Print time spent for execution
