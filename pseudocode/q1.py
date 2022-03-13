e236f822859cf43695987ef8ddec671514931b72
theomper <theo.mper@gmail.com>
2022-03-14 00:24:17 +0200

    [major] - added pseucode for q1 and q2

---
+++ pseudocode/q1.py
@@ -0,0 +1,25 @@
+# movies = movies.csv
+
+map(movies, value):
+    for movie in movies:
+        line = movie.split(',')
+        # movie = (movie_id, title, description, publish_date,
+        #          duration, cost, income, favoured)
+        title = line[1]
+        year = line[3].split('-')[0]
+        publish_date = line[3]
+        cost = line[5]
+        income = line[6]
+        if ((publish_date != '') and (cost != 0) and
+            (income != 0) and (year > 2000)):
+            profit = ((income - cost) / cost) * 100
+            emit(year, (title, profit))
+
+reduce(year, [(title, profit)]):
+    max_profit = 0
+    max_title = ''
+    for title, profit in [(title, profit)]:
+        if (max_profit > profit):
+            max_profit = profit
+            max_title = title
+    emit(year, (max_title, max_profit))
---
+++ pseudocode/q2.py
@@ -0,0 +1,28 @@
+# ratings = ratings.csv
+
+map(ratings, value):
+    for movie in movies:
+        line = movie.split(',')
+        # ratings = (user_id, movie_id, rating, timestamp))
+        user_id = line[0]
+        rating = line[2]
+        emit(user_id, (rating, 1))
+
+reduce(user_id, [(rating, 1)]):
+    sum_of_ratings = 0
+    cnt_of_movies = ''
+    for rating, cnt in [(rating, 1)]:
+        sum_of_ratings += rating
+        cnt_of_movies++
+    emit(user_id, (sum_of_ratings, cnt_of_movies))
+
+
+map(user_id, (sum_of_ratings, cnt_of_movies)):
+    avg_rating = sum_of_ratings / cnt_of_movies
+    emit(user_id, avg_rating)
+
+## TODO Pythonwise or M-Rwise??
+all_users = ([(user_id, avg_rating), (,), ...]).count()
+users_above_3 = ([(user_id, avg_rating), (,), ...]).filter(avg_rating >= 3.0)
+
+percentage = users_above_3 / all_users * 100
