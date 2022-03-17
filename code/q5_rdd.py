2e8b0e6fd4cef6c9c6db621e577b94e97e6e479f
theomper <theo.mper@gmail.com>
2022-03-17 22:58:55 +0200

    [major] - q5_rdd done

--- code/q5_rdd.py
+++ code/q5_rdd.py
@@ -1,7 +1,6 @@
 # code for capturing execution time copied from
 # https://stackoverflow.com/questions/1557571/how-do-i-get-time-of-a-python-programs-execution
 
-from re import L
 from pyspark.sql import SparkSession
 from io import StringIO
 import csv
@@ -14,7 +13,7 @@ def split_complex(x):
 # Start clock
 start_time = time.time()
 
-spark = SparkSession.builder.appName("q4_rdd").getOrCreate()
+spark = SparkSession.builder.appName("q5_rdd").getOrCreate()
 sc = spark.sparkContext
 
 # Inputs - Movies, Genres and Ratings
@@ -28,14 +27,13 @@ movies = \
 # map => (movie_id, genre)
 genres = \
     sc.textFile("hdfs://master:9000/files/movie_genres.csv"). \
-    map(lambda x: split_complex(x))
+    map(lambda x: (x.split(',')[0], x.split(',')[1])).distinct()
 
 # map => (user_id, movie_id, rating, timestamp)
 # map => (movie_id, (user_id, rating))
 ratings = \
     sc.textFile("hdfs://master:9000/files/ratings.csv"). \
-    map(lambda x: split_complex(x)). \
-    map(lambda x: (x[1], (x[0], float(x[2]))))
+    map(lambda x: (x.split(',')[1], (x.split(',')[0], float(x.split(',')[2]))))
 
 # Joins
 # join => (movie_id, (genre, (title, favoured)))
@@ -43,15 +41,28 @@ ratings = \
 temp_join = genres.join(movies). \
     map(lambda x: (x[0], (x[1][0], x[1][1][0], x[1][1][1])))
 
-# join => (movie_id, (genre, title, favoured), (user_id, rating))
-final_join = temp_join.join(ratings)
-
 # Processing
-# TODO
+# join => (movie_id, ((genre, title, favoured), (user_id, rating)))
+# map => ((user_id, genre), (1, title, rating, favoured, title, rating, favoured))
+# reduce => ((user_id, genre), (count, max_title, max_rating, max_favoured, min_title, min_rating, min_favoured))
+# map => (genre, (user_id, count, max_title, max_rating, min_title, min_rating))
+# reduce => (genre, (user_id, max_count, max_title, max_rating, min_title, min_rating))
+# map => (genre, user_id, count, max_title, max_rating, min_title, min_rating))
+final_join = temp_join.join(ratings).map(lambda x: ((x[1][0][0], x[1][1][0]), (1, x[1][0][1], x[1][1][1], x[1][0][2], x[1][0][1], x[1][1][1], x[1][0][2]))). \
+    reduceByKey(lambda x, y: (x[0]+y[0], x[1] if x[2]>y[2] else (y[1] if y[2]>x[2] else (x[1] if x[3]>y[3] else y[1])),
+                                        x[2] if x[2]>y[2] else (y[2] if y[2]>x[2] else (x[2] if x[3]>y[3] else y[2])),
+                                        x[3] if x[2]>y[2] else (y[3] if y[2]>x[2] else (x[3] if x[3]>y[3] else y[3])),
+                                        x[4] if x[5]<y[5] else (y[4] if y[5]<x[5] else (x[4] if x[6]>y[6] else y[4])),
+                                        x[5] if x[5]<y[5] else (y[5] if y[5]<x[5] else (x[5] if x[6]>y[6] else y[5])),
+                                        x[6] if x[5]<y[5] else (y[6] if y[5]<x[5] else (x[6] if x[6]>y[6] else y[6])))). \
+    map(lambda x: (x[0][0], (x[0][1], x[1][0], x[1][1], x[1][2], x[1][4], x[1][5]))). \
+    reduceByKey(lambda x, y: (x[0], x[1], x[2], x[3], x[4], x[5]) if x[1]>y[1] else (y[0], y[1], y[2], y[3], y[4], y[5])). \
+    sortByKey(ascending=True). \
+    map(lambda x: (x[0], x[1][0], x[1][1], x[1][2], x[1][3], x[1][4], x[1][5]))
 
 # Outputs
-# for result in results.collect():
-#     print(result)
+for result in final_join.collect():
+    print(result)
 
 # Print time spent for execution
 print("---Completed in %s seconds ---" % (time.time() - start_time))
